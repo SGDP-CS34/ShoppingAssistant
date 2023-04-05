10b793172ef11c6b33d9f509f1f80de76366f526
kvasheilds <kulithvinwara119@gmail.com>
2023-04-05 21:54:43 +0530

    get images

--- ML model/train.py
+++ ML model/train.py
@@ -10,3 +10,34 @@ import pandas as pd
 import os
 import json
 
+DATASET_PATH = 'data'
+# Define the number of rows to read, set to 500 because
+# the first 500 rows were added to the database used by the
+# API
+numberOfRowsToRead = 500
+# Define the image size
+img_width, img_height = 224, 224
+
+
+# Gets the path to the image
+def img_path(img):
+    return DATASET_PATH+"/images/"+img
+
+
+# Gets the embedding for a given image using the model
+def get_embedding(model, img_name):
+    # Reshape
+    img = image.load_img(img_path(img_name),
+                         target_size=(img_width, img_height))
+    # img to Array
+    x = image.img_to_array(img)
+    # Expand Dim (1, w, h)
+    x = np.expand_dims(x, axis=0)
+    # Pre process Input
+    x = preprocess_input(x)
+    return model.predict(x).reshape(-1)
+
+
+
+
+
